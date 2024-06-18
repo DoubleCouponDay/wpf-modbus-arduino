33d0c6d87e77fa2b259ae84cd17e7648e99eb7d4
Doublecouponday <mail@samueljenks.me>
2024-06-18 17:06:07 +1200

    step: modbus interface

---
+++ wpf-modbus-webview2/Modbus.cs
@@ -0,0 +1,48 @@
+﻿using FluentModbus;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_modbus_webview2
+{
+    class Modbus
+    {
+
+        IModbusBridge bridge;
+
+        public Modbus(IModbusBridge inputBridge)
+        {
+            bridge = inputBridge;
+        }
+    }
+
+    interface IModbusBridge
+    {
+        void ModbusRequest(string command);
+        Task<string> ModbusResponse();
+    }
+
+    class ModbusBridge : IModbusBridge
+    {
+        ModbusTcpServer server;
+
+        public ModbusBridge(ILogger logger, IPAddress sawAddress, IPAddress trolleyAddress)
+        {
+            server = new ModbusTcpServer(logger);
+            server.Start();
+        }
+        public void ModbusRequest(string command)
+        {
+            server.
+        }
+
+        public Task<string> ModbusResponse()
+        {
+            
+        }
+    }
+}
---
+++ wpf-modbus-webview2/README.md
@@ -0,0 +1,7 @@
+# wpf-modbus-webview2
+Example application of an HMI for servo motor automation
+
+# Links
+
+- [FluentModbus](https://apollo3zehn.github.io/FluentModbus/samples/modbus_tcp.html)
+- 
