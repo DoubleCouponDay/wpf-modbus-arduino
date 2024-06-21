7c7d5864e13ba0a86299f77018376a2d67e0aa56
Doublecouponday <mail@samueljenks.me>
2024-06-21 14:55:55 +1200

    step: navigationwindow

--- wpf-modbus-webview2/MainWindow.xaml.cs
+++ wpf-modbus-webview2/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace wpf_modbus_webview2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
